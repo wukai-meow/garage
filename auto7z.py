fdcf5ddd6ea016609d4dfd7d8acbd4393d115755
Kai Wu <alonewk@gmail.com>
2022-08-19 10:29:57 +0800

    文件移动逻辑；支持windows

--- auto7z.py
+++ auto7z.py
@@ -4,12 +4,14 @@ import sys
 from subprocess import run
 from glob import glob
 
+
 def get_output(cmd: str, raise_error=False) -> list:
     '''
     get output of a shell cmd. ignore error by default
     return: list of strings (one line per item, no \\n in the end)
     '''
     return str(run(cmd, shell=True, check=raise_error, capture_output=True).stdout, encoding='utf-8').split("\n")[:-1]
+
 
 def update_toUnzipList():
     compress_file_ext = ('7z', 'zip', 'rar', 'tar.gz', 'tar', 'tgz')
@@ -21,68 +23,78 @@ def update_toUnzipList():
     return to_unzip
 
 
+if __name__ == "__main__":
+    my_dir = os.path.abspath(os.path.dirname(__file__))
+    passwordfile = my_dir + os.sep + "passwords.txt"
 
-my_dir = os.path.abspath(os.path.dirname(__file__))
-passwordfile = my_dir + "/passwords.txt"
+    if not os.path.isfile(passwordfile):
+        with open(passwordfile, 'w') as f:
+           f.write("\n")
 
-if not os.path.isfile(passwordfile):
-    with open(passwordfile, 'w') as f:
-        f.write("\n")
+    # print(get_output("cat passwords.txt"))
+    with open(passwordfile, 'r') as f:
+       passwords = f.readlines()
 
-# print(get_output("cat passwords.txt"))
-with open(passwordfile, 'r') as f:
-    passwords = f.readlines()
+    if not os.path.isdir("extracted"):
+       os.mkdir("extracted")
 
-if not os.path.isdir("extracted"):
-    os.mkdir("extracted")
-
-nExtracted = 0
-to_unzip = update_toUnzipList()
-total_nToUnzip = 0
-while len(to_unzip) != 0:
-    total_nToUnzip += len(to_unzip)
-    for fn in to_unzip:
-        print(f"{nExtracted+1}/{total_nToUnzip}\t{fn}")
-        nsavedpwd = len(passwords)
-        need_new_pwd = False
-        ipwdtry = 0
-        output = ''
-        while not "Everything is Ok" in ''.join(output):
-            if ipwdtry < nsavedpwd:
-                sys.stdout.write("\r")
-                sys.stdout.write(f"Trying password {ipwdtry} / {nsavedpwd}")
-                sys.stdout.flush()
-                pwd = passwords[ipwdtry][:-1]
-                cmd = f"7z x '{fn}' -p{pwd} -r -aoa -o'{os.path.splitext(fn)[0]}'"
-                # print(cmd)
-                output = get_output(cmd)
-                ipwdtry += 1
-            else:
-                need_new_pwd = True
-                break
-        if need_new_pwd:
-            while not "Everything is Ok" in ''.join(output):
-                pwd = input("Enter password: ")
-                cmd = f"7z x '{fn}' -p{pwd} -r -aoa -o'{os.path.splitext(fn)[0]}'"
-                print(cmd)
-                output = get_output(cmd)
-            passwords.append(pwd + '\n')
-        os.rename(fn, "extracted/"+fn)
-
-        if len(os.listdir(os.path.splitext(fn)[0])) < 10:
-            get_output(f"mv '{os.path.splitext(fn)[0]}'/* ./ && trash '{os.path.splitext(fn)[0]}'")
-        nExtracted += 1
-        print("  Succeeded.")
-
+    nExtracted = 0
     to_unzip = update_toUnzipList()
+    total_nToUnzip = 0
+    while len(to_unzip) != 0:
+        total_nToUnzip += len(to_unzip)
+        for fn in to_unzip:
+            print(f"{nExtracted+1}/{total_nToUnzip}\t{fn}")
+            nsavedpwd = len(passwords)
+            need_new_pwd = False
+            ipwdtry = 0
+            output = ''
+            while not "Everything is Ok" in ''.join(output):
+                if ipwdtry < nsavedpwd:
+                    sys.stdout.write("\r")
+                    sys.stdout.write(
+                        f"Trying password {ipwdtry} / {nsavedpwd}")
+                    sys.stdout.flush()
+                    pwd = passwords[ipwdtry][:-1]
+                    cmd = f"7z x '{fn}' -p{pwd} -r -aoa -o'{os.path.splitext(fn)[0]}'"
+                    # print(cmd)
+                    output = get_output(cmd)
+                    ipwdtry += 1
+                else:
+                    need_new_pwd = True
+                    break
+            if need_new_pwd:
+                while not "Everything is Ok" in ''.join(output):
+                    pwd = input("Enter password: ")
+                    cmd = f"7z x '{fn}' -p{pwd} -r -aoa -o'{os.path.splitext(fn)[0]}'"
+                    print(cmd)
+                    output = get_output(cmd)
+                passwords.append(pwd + '\n')
+            os.rename(fn, "extracted" + os.sep +fn)
 
-with open(passwordfile, 'w') as f:
-    f.write(''.join(passwords))
+            nfile = 0
+            for root_dir, cur_dir, files in os.walk(os.path.splitext(fn)[0]):
+                nfile += len(files)
+            
+            if nfile <= 5:
+                for root_dir, cur_dir, files in os.walk(os.path.splitext(fn)[0], topdown=False):
+                    for _f in files:
+                        os.rename(root_dir + os.sep + _f,
+                                  os.getcwd() + os.sep + _f)
+                    os.rmdir(root_dir)
 
-print('\n'.join(get_output("df -h `pwd`")))
+            nExtracted += 1
+            print("  Succeeded.")
 
-# print(f"{nExtracted} files extracted.")
+        to_unzip = update_toUnzipList()
 
-# print(get_output("cat passwords.txt"))
+    with open(passwordfile, 'w') as f:
+        f.write(''.join(passwords))
+
+    print('\n'.join(get_output("df -h `pwd`")))
+
+    # print(f"{nExtracted} files extracted.")
+
+    # print(get_output("cat passwords.txt"))
 
 
